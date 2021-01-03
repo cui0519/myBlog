f0f50670f7e63b03305acc83270c14be97ff6a28
Your Name <you@example.com>
2021-01-03 21:25:34 +0800

    '数据更改'

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Articles
 # Register your models here.
+
+class ArticlesAdmin(admin.ModelAdmin):
+
+    list_display = ('title','author','img','abstract','visited','created_at')
+    
+    search_fields = ('title','author','abstract','content')
+    
+    list_filter = list_display
+
+
+admin.site.register(Articles,ArticlesAdmin)
